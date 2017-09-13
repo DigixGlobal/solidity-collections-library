ae806e56780bd41e39cd19bbf45caa7a4999e497
Anthony Eufemio <ace@dgx.io>
2017-09-13 16:52:08 +0800

    add support for indexed collections

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,23 +7,33 @@ var ExampleStorage = artifacts.require("./ExampleStorage.sol");
 var ExampleController = artifacts.require("./ExampleController.sol");
 var ExampleInteractive = artifacts.require("./ExampleInteractive.sol");
 
+var ExampleIndexedStorage = artifacts.require("./ExampleIndexedStorage.sol");
+var ExampleIndexedController = artifacts.require("./ExampleIndexedController.sol");
+var ExampleIndexedInteractive = artifacts.require("./ExampleIndexedInteractive.sol");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(DoublyLinkedList).then(() => {
     deployer.link(DoublyLinkedList, TestDoublyLinkedList);
     deployer.link(DoublyLinkedList, TestLibraryUint);
     deployer.link(DoublyLinkedList, TestLibraryAddress);
-    deployer.link(DoublyLinkedList, TestLibraryBytes);
+    return deployer.link(DoublyLinkedList, TestLibraryBytes);
+  }).then(() => {
+    deployer.link(DoublyLinkedList, ExampleIndexedStorage);
     return deployer.link(DoublyLinkedList, ExampleStorage);
   }).then(() => {
     deployer.deploy(TestLibraryBytes);
     deployer.deploy(TestLibraryAddress);
-    deployer.deploy(TestLibraryUint);
+    return deployer.deploy(TestLibraryUint);
+  }).then(() => {
     deployer.deploy(ExampleStorage);
+    deployer.deploy(ExampleIndexedStorage);
     return deployer.deploy(TestDoublyLinkedList);
   }).then(() => {
+    ExampleIndexedStorage.at(ExampleIndexedStorage.address).initialize();
+    deployer.deploy(ExampleIndexedController, ExampleIndexedStorage.address);
     return deployer.deploy(ExampleController, ExampleStorage.address);
   }).then(() => {
+    deployer.deploy(ExampleIndexedInteractive, ExampleIndexedController.address);
     return deployer.deploy(ExampleInteractive, ExampleController.address);
   });
 };
